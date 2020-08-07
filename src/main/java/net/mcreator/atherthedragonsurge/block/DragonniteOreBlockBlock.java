96e6abef6c11173bb1b2bb15335455ce59063bd6
Zafrax <batza@DESKTOP-KIV714P>
2020-08-07 01:24:56 -0500

    Updated Boots Now Fixed (Hopefully) and added a nethernite ingot v0.0.2

---
+++ src/main/java/net/mcreator/atherthedragonsurge/block/DragonniteOreBlockBlock.java
@@ -0,0 +1,81 @@
+
+package net.mcreator.atherthedragonsurge.block;
+
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.common.ToolType;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.world.storage.loot.LootContext;
+import net.minecraft.world.IBlockReader;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.item.BlockItem;
+import net.minecraft.client.renderer.RenderTypeLookup;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import net.mcreator.atherthedragonsurge.item.DragonniteIngotItem;
+import net.mcreator.atherthedragonsurge.AtherTheDragonSurgeModElements;
+
+import java.util.List;
+import java.util.Collections;
+
+@AtherTheDragonSurgeModElements.ModElement.Tag
+public class DragonniteOreBlockBlock extends AtherTheDragonSurgeModElements.ModElement {
+	@ObjectHolder("ather_the_dragon_surge:dragonnite_ore_block")
+	public static final Block block = null;
+	public DragonniteOreBlockBlock(AtherTheDragonSurgeModElements instance) {
+		super(instance, 12);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new CustomBlock());
+		elements.items
+				.add(() -> new BlockItem(block, new Item.Properties().group(ItemGroup.BUILDING_BLOCKS)).setRegistryName(block.getRegistryName()));
+	}
+
+	@Override
+	@OnlyIn(Dist.CLIENT)
+	public void clientLoad(FMLClientSetupEvent event) {
+		RenderTypeLookup.setRenderLayer(block, RenderType.getCutout());
+	}
+	public static class CustomBlock extends Block {
+		public CustomBlock() {
+			super(Block.Properties.create(Material.IRON).sound(SoundType.GLASS).hardnessAndResistance(5f, 10f).lightValue(0).harvestLevel(6)
+					.harvestTool(ToolType.PICKAXE).notSolid());
+			setRegistryName("dragonnite_ore_block");
+		}
+
+		@Override
+		public boolean isNormalCube(BlockState state, IBlockReader worldIn, BlockPos pos) {
+			return false;
+		}
+
+		@Override
+		public boolean propagatesSkylightDown(BlockState state, IBlockReader reader, BlockPos pos) {
+			return true;
+		}
+
+		@Override
+		public boolean isReplaceable(BlockState state, BlockItemUseContext context) {
+			return true;
+		}
+
+		@Override
+		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+			if (!dropsOriginal.isEmpty())
+				return dropsOriginal;
+			return Collections.singletonList(new ItemStack(DragonniteIngotItem.block, (int) (1)));
+		}
+	}
+}
